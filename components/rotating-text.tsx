569e2b7a675399dad4263adaac1be8fe56f7b823
Darryl Brooks <darrylbrooks13@google.com>
2021-10-03 12:35:32 -0500

    fix: typed rotating-text component

--- components/rotating-text.tsx
+++ components/rotating-text.tsx
@@ -2,14 +2,14 @@ import { Box, Text } from '@chakra-ui/react';
 import { useEffect } from 'react';
 
 const temp = () => {
-	var words = document.getElementsByClassName('word');
-	var wordArray = [];
+	var words: any = document.getElementsByClassName('word');
+	var wordArray: HTMLSpanElement[][] = [];
 	var currentWord = 0;
 
-	const splitLetters = (word) => {
+	const splitLetters = (word: any) => {
 		var content = word.innerHTML;
 		word.innerHTML = '';
-		var letters = [];
+		var letters: any = [];
 
 		for (var i = 0; i < content.length; i++) {
 			var letter = document.createElement('span');
@@ -22,7 +22,7 @@ const temp = () => {
 		wordArray.push(letters);
 	};
 
-	words[currentWord].style.opacity = 1;
+	words[currentWord].style.opacity = '1';
 	words[0].style.position = 'relative';
 	console.log(words[currentWord]);
 
@@ -31,8 +31,8 @@ const temp = () => {
 	}
 
 	const changeWord = () => {
-		var cw = wordArray[currentWord];
-		var nw =
+		var cw: any = wordArray[currentWord];
+		var nw: any =
 			currentWord == words.length - 1
 				? wordArray[0]
 				: wordArray[currentWord + 1];
@@ -43,20 +43,20 @@ const temp = () => {
 		}
 		for (var i = 0; i < nw.length; i++) {
 			nw[i].className = 'letter behind';
-			nw[0].parentElement.style.opacity = 1;
+			nw[0].parentElement.style.opacity = '1';
 			animateLetterIn(nw, i);
 		}
 
 		currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
 	};
 
-	const animateLetterOut = (cw, i) => {
+	const animateLetterOut = (cw: any, i: number) => {
 		setTimeout(function () {
 			cw[i].className = 'letter out';
 		}, i * 80);
 	};
 
-	const animateLetterIn = (nw, i) => {
+	const animateLetterIn = (nw: any, i: number) => {
 		setTimeout(function () {
 			nw[i].className = 'letter in';
 		}, 320 + i * 80);
