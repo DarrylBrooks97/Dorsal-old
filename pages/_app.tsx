d965b2ed8b78185b4aaf8ba893039a5321472fc0
Darryl Brooks <darrylbrooks13@google.com>
2021-10-09 13:34:32 -0500

    feat: added Default SEO to main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,39 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import { theme } from '../theme';
+import { DefaultSeo } from 'next-seo';
+import { canonicalUrl } from '../constants';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
+			<DefaultSeo
+				defaultTitle="Dorsal 🦈"
+				description="An platform for aquarium enthusiasts"
+				canonical={canonicalUrl}
+				additionalLinkTags={[
+					{
+						rel: 'icon',
+						href: `${canonicalUrl}/favicon.ico`,
+					},
+				]}
+				openGraph={{
+					type: 'website',
+					url: `${canonicalUrl}`,
+					site_name: 'Dorsal 🦈',
+					images: [
+						{
+							url: `${canonicalUrl}/seo_banner.png`,
+							width: 800,
+							height: 450,
+							alt: 'Dorsal Banner',
+						},
+					],
+				}}
+				twitter={{
+					cardType: 'summary_large_image',
+				}}
+			/>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
