2104b5d3733e93f56d321614c9120706ef8f89f0
Darryl Brooks <24500251+DarrylBrooks97@users.noreply.github.com>
2021-10-09 13:55:07 -0500

    fix: fixing incorrect url

--- constants.ts
+++ constants.ts
@@ -3,5 +3,5 @@ export const canonicalUrl: string =
 	process.env.NEXT_PUBLIC_TARGET_ENV === 'prod'
 		? 'https://www.dorsal.vercal.app/'
 		: process.env.NEXT_PUBLIC_TARGET_ENV === 'staging'
-		? 'http://www.staging-dorsal.vercal.app/'
+		? 'https://www.staging-dorsal.vercal.app/'
 		: 'http://localhost:3000/';
