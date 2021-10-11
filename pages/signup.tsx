d9a6ad2f8591825fd41b4de17ab179aaf030c9b0
Darryl Brooks <darrylbrooks13@google.com>
2021-10-10 20:05:47 -0500

    feat: added icons to buttons

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,7 @@
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 import { Box, VStack, Flex, Input, Button, Text } from '@chakra-ui/react';
+import { FcGoogle } from 'react-icons/fc';
+import { BsTwitter, BsFacebook } from 'react-icons/bs';
 
 export default function SignUp(): ReactJSXElement {
 	return (
@@ -11,13 +13,27 @@ export default function SignUp(): ReactJSXElement {
 			justifyContent="center"
 			bg="brand.offwhite"
 		>
-			<Flex w="300px" h="600px" /*bg="blue"*/ p="10px">
-				<VStack /*bg="green"*/ w="100%">
+			<Flex
+				w="300px"
+				h="600px"
+				/*bg="blue"*/ p="10px"
+				// bg="rgba(136, 150, 150, 0.8)"
+				borderRadius="25px"
+			>
+				<VStack /*bg="green"*/ w="100%" p="2px">
 					<Text fontSize="6xl">Dorsal 🦈</Text>
-					<Text fontSize="1.3125rem">Become an Aquarist Today</Text>
+					<Text fontSize="1.3125rem">Become an Aquarist today</Text>
 					<VStack w="100%">
-						<Input placeholder="Email address" w="100%" />
-						<Input placeholder="Password" w="100%" />
+						<Input
+							placeholder="Email address"
+							w="100%"
+							_focus={{ bordercolor: 'brand.green' }}
+						/>
+						<Input
+							placeholder="Password"
+							w="100%"
+							_focus={{ borderColor: 'brand.green' }}
+						/>
 						<Button
 							bg="brand.green"
 							color="brand.offwhite"
@@ -29,9 +45,15 @@ export default function SignUp(): ReactJSXElement {
 					</VStack>
 					<hr />
 					<VStack w="100%" p="50px 0px">
-						<Button w="100%">Continue with Google</Button>
-						<Button w="100%">Continue with Facebook</Button>
-						<Button w="100%">Continue with Twitter</Button>
+						<Button leftIcon={<FcGoogle />} w="100%">
+							Continue with Google
+						</Button>
+						<Button leftIcon={<BsFacebook />} w="100%">
+							Continue with Facebook
+						</Button>
+						<Button leftIcon={<BsTwitter />} w="100%">
+							Continue with Twitter
+						</Button>
 					</VStack>
 				</VStack>
 			</Flex>
